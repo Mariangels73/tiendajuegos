48e3d7bf15ed23005f636f4a9521ca0bf60db187
Maria de los Angeles Loyo <110129762+Mariangels73@users.noreply.github.com>
2024-06-09 16:13:04 -0400

    desafio de tienda de videojuegos

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Tienda de Juegos 32 Bits</h1>
+    <h2>Lista de juegos</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Nombre</th>
+          <th>Stock</th>
+          <th>Precio</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(juego, index) in juegos"
+          :key="juego.codigo"
+        >
+          <td>{{ juego.codigo }}</td>
+          <td>{{ juego.nombre }}</td>
+          <td>{{ juego.stock }}</td>
+          <td>${{ Number(juego.precio).toLocaleString() }}</td>
+          <td>
+            <button @click="incrementar(juego.codigo)">+</button>
+            <button @click="decrementar(juego.codigo)">-</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState, mapActions } from 'vuex';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    ...mapState(['juegos'])
+  },
+  methods: {
+    ...mapActions(['incrementarStock', 'decrementarStock']),
+    incrementar(codigo) {
+      this.incrementarStock(codigo);
+    },
+    decrementar(codigo) {
+      this.decrementarStock(codigo);
+    }
   }
 }
 </script>
@@ -22,5 +58,40 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+table {
+  margin: 0 auto;
+  width: 80%; /* Ajusta el ancho de la tabla según sea necesario */
+}
+
+th, td {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+th {
+  background-color: #f2f2f2;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+button {
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 2px 0px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #45a049;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,35 @@
-import { createStore } from 'vuex'
+// index.js
+import { createStore } from 'vuex';
+import juegos from "./juegos.json";
 
 export default createStore({
   state: {
+    juegos
   },
   getters: {
   },
   mutations: {
+    INCREMENTAR_STOCK(state, codigo) {
+      const juego = state.juegos.find(j => j.codigo === codigo);
+      if (juego) {
+        juego.stock++;
+      }
+    },
+    DECREMENTAR_STOCK(state, codigo) {
+      const juego = state.juegos.find(j => j.codigo === codigo);
+      if (juego && juego.stock > 0) {
+        juego.stock--;
+      }
+    }
   },
   actions: {
+    incrementarStock({ commit }, codigo) {
+      commit('INCREMENTAR_STOCK', codigo);
+    },
+    decrementarStock({ commit }, codigo) {
+      commit('DECREMENTAR_STOCK', codigo);
+    }
   },
   modules: {
   }
-})
+});
